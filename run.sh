6a86458663e2611df3c35feea91dfe7a375713c9
Thiago Furtado <thiagofurtado17@gmail.com>
2012-07-01 21:16:33 -0300

    generated reports for FRESH routing

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#./one.sh -b 3 cenarios/andre_mesquita/settings.txt
+#./one.sh -b 3 cenarios/everton_almir/settings.txt
+#./one.sh -b 3 cenarios/piassi_bodo/settings.txt
+#./one.sh -b 3 cenarios/thiago_jose/settings.txt
+#./one.sh -b 3 cenarios/cassio_patricia_roberto/settings.txt
+#./one.sh -b 3 cenarios/frank_aguilar/settings.txt
+./one.sh -b 3 cenarios/serafim_vieira/settings.txt
+./one.sh -b 3 cenarios/damian_guilherme/settings.txt
+#./one.sh -b 3 cenarios/gabriel/settings.txt
+./one.sh -b 3 cenarios/tasso_tania/settings.txt
